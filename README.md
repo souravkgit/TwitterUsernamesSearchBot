59047ea0cec00aa5183ea947967f41e119c66db1
Sourav Goyal <78165168+souravkgit@users.noreply.github.com>
2021-12-29 12:23:38 +0530

    Uploading python code for tele bot
    Python Twitter username searching bot code

--- README.md
+++ README.md
@@ -1,2 +1,80 @@
-# TwitterUsernamesSearchBot
-Helps to search availability of any username on twitter
+# What is this repo about?
+This is a [telegram bot](https://t.me/TwitterUsernameSearchBot) written in python for Searching availability of any username on twitter and it's deployed on [Heroku](https://heroku.com).
+
+# Inspiration 
+This project is heavily inspired from [@goyalcompany's](https://t.me/goyalcompany) request.
+
+# Features supported:
+- Can search Multiple usernames at once
+- Can convert any username into twitter link
+- Can Stop any ongoing function on user request
+- More features in future
+
+# How to deploy?
+Deploying is pretty much straight forward and is divided into several steps as follows:
+## Installing requirements
+
+- Clone this repo:
+```
+git clone https://github.com/souravkgit/TwitterUsernamesSearchBot/
+```
+
+- Install requirements
+For Debian based distros
+```
+sudo apt install python3
+```
+or
+
+Install python from [Here](https://www.python.org/)
+- Install dependencies for running setup scripts:
+```shell script
+pip3 install -r requirements.txt
+```
+## Procfile
+- If you want to deploy your bot on Heroku then you can create this Procfile  , You just have to change File name with your file name like
+```
+worker: python3 bot.py -----> worker: python3 <your file name>.py
+```
+
+## Setting up config file
+```
+cp config.ini
+```
+Fill up all the fields given below . Meaning of each fields are discussed below:
+- **Token** : The telegram bot token that you get from @BotFather
+- **consumer_key** : Twitter Developer Account consumer key , For this you have to create a Twitter developer account.
+- **consumer_secret** : Twitter Developer Account consumer key secret , For this you have to create a Twitter developer account.
+- **access_token** : Twitter Developer Account app access token , For this you have to create a Twitter developer account.  
+- **access_token_secret** : Twitter Developer Account app access token secret , For this you have to create a Twitter developer account.
+
+## Getting Twitter Developer Auth credentials
+
+- Visit the [Twitter Developer Platform](https://developer.twitter.com/en/docs/twitter-api/getting-started/getting-access-to-the-twitter-api)
+- Sign up for a developer account by filling just few basic details.
+- Agree for the Developer agreement & policy and Submit and then confirm your mail.
+- After verifying mail create an app by putting name and hit get keys.
+- Save your keys safely (if lost you have to generate new ones).
+- Then come to your profile dashboard and then go on your project/app.
+- Then go on keys and tokens and then generate **Access Token and Secret**.
+- Then it will pop-up all of your API keys and secrets , save them all carefully.
+- May be you need to apply for elevated access for this code so if that's the case then follow these steps .
+- [Go here](https://developer.twitter.com/en/portal/products/elevated) and apply for Elevated access.
+- Fill few details and it's done.
+- It will take few minutes to verify and then it's done ,You can use it perfectly.
+- Done!!
+
+## Deploying
+
+- Just upload or push your code to a github repository
+- Connect your github to your heroku and deploy that repo to your heroku app
+```
+
+OR
+```
+- Just go [Here](https://www.freecodecamp.org/news/how-to-deploy-a-nodejs-app-to-heroku-from-github-without-installing-heroku-on-your-machine-433bec770efe/) and Read from **Step2**.
+
+
+- Congrats it's done your own bot is now working perfectly fine.
+
+## Thanks for reading this.
---
+++ bot.py
@@ -0,0 +1,125 @@
+#! /usr/bin/python3
+# Twitter username bot , coded by @souravkkk or Sourav Goyal
+
+
+# Import these libraries from python
+
+import telebot
+import time
+import tweepy as tw
+import configparser
+
+# Reading our config file for credentials.
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+# Defining keys using credentials from config file.
+TOKEN = config['Telegram']['Token']
+consumer_key = config['Twitter']['consumer_key']
+consumer_secret = config['Twitter']['consumer_secret']
+access_token = config['Twitter']['access_token']
+access_token_secret = config['Twitter']['access_token_secret']
+
+# Defining basic details that will be used in code. 
+RUNI = []
+bot = telebot.TeleBot(TOKEN)
+
+# Defining function to convert user text into args
+def getargs(text):
+    _args = text.split()[1:]
+    return _args
+
+# Code for bot commands start from here ....
+
+# First command for /start button 
+@bot.message_handler(commands=["start"])
+def sayhello(message):
+    RUNI.clear()
+    bot.send_message(message.chat.id,"Hello Hooman , Welcome to my Bot\n Use Given Commands to explore me!")
+
+# Second command for /help button
+@bot.message_handler(commands=["help"])
+def sayhelp(message):
+    bot.send_message(message.chat.id,"What you find tough so you seek help hooman??")
+
+# Third command for /stop button ---> Helps to stop any ongoing function
+@bot.message_handler(commands=["st","stop"])
+def stop(message):
+    RUNI.pop()
+    RUNI.append("stop")
+
+# Fourth command for /link button ---> Helps to create twitter link for any twitter username
+@bot.message_handler(commands=["l","link"])
+def nametolink(message):
+    RUNI.append("run")
+    args = getargs(message.text)
+    text = " ".join(args[0:])
+    if len(text)>300:
+        bot.send_message(message.chat.id,"Whoaa kid! Have some patience and give a smaller message to convert!")
+        return
+    text = list(text.split())
+    if (len(text)==0):
+        bot.send_message(message.chat.id,"Whoaa! kid atleast give one username to change!!")
+        return 
+    for each in text:
+        if (RUNI[-1] == "stop"):
+            bot.send_message(message.chat.id,"Okay Sire!! Stopping bot as you said!!")
+            RUNI.pop()
+            time.sleep(4)
+            break 
+        if (each[0]=='@'):
+            each = "https://twitter.com/" + each[1:]
+        elif(each[0:5]=="https"):
+            bot.send_message(message.chat.id,"Whoaa! Hooman this is already a link!")
+        else:
+            each = "https://twitter.com/" + each
+        try:
+            bot.send_message(message.chat.id,each)
+        except Exception as e:
+            bot.send_message(message.chat.id,f"What u Wanna find Nigga?? .... {e}")
+
+# Fifth command for /search button ---> Helps to search for availability of any username on twitter
+@bot.message_handler(commands=["s","search"])
+def nametolink(message):
+    RUNI.append("run")
+    args = getargs(message.text)
+    text = " ".join(args[0:])
+    if len(text)>800:
+        bot.send_message(message.chat.id,"Whoaa kid! Have some patience and give some less usernames to search!!")
+        RUNI.pop()
+        return
+    text = list(text.split())
+    remain = []
+    if (len(text)==0):
+        bot.send_message(message.chat.id,"Whoaa! kid atleast give one username to search!!")
+        return
+    auth = tw.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    api = tw.API(auth, wait_on_rate_limit=True)
+    for each in text:
+        if (RUNI[-1] == "stop"):
+            bot.send_message(message.chat.id,"Okay Sire!! Stopping bot as you said!!")
+            time.sleep(2)
+            break
+        try:
+            user = api.get_user(screen_name=each)
+            printi = f"👤Username : {each}\n\n✅Status : Exist"
+            bot.send_message(message.chat.id,printi)
+        except:
+            printi = f"👤Username : {each}\n\n❌Status : Doesn't Exist"
+            remain.append(each)
+            bot.send_message(message.chat.id,printi)
+    if (len(remain)==0):
+        bot.send_message(message.chat.id,"This Bot is created by @souravkkk")
+        return 
+    bot.send_message(message.chat.id,"The usernames Which Don't exist are :\n")
+    for each in remain:
+        bot.send_message(message.chat.id,each)
+    bot.send_message(message.chat.id,"This Bot is created by @souravkkk")
+
+# For polling of bot.
+while True:
+    try:
+        bot.polling()
+    except:
+        time.sleep(5)
